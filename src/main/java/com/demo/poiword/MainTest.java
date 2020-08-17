1d424defb0a3366855390e36086a7bfe5af4c42e
mymy <bianminana@labuchushi.com>
2020-08-17 10:02:36 +0800

    commit 2

--- src/main/java/com/demo/poiword/MainTest.java
+++ src/main/java/com/demo/poiword/MainTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.demo.poiword;
 
 
@@ -79,31 +78,3 @@ public class MainTest {
 
 
 }
-=======
-package com.demo.poiword;
-
-
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.HashMap;
-import java.util.Map;
-
-@RestController
-public class MainTest {
-    @Autowired
-    private WordUtils wordUtils;
-
-    public void exportWord(HttpServletResponse response) throws IOException, InvalidFormatException {
-        String path="";
-        Map<String,Object> params=new HashMap<>();
-        String filename="";
-        wordUtils.exportWord(path,params,filename,response);
-
-    }
-}
->>>>>>> 620d3b1a5311b01f5fd49b3fdc870b3d93f23318
